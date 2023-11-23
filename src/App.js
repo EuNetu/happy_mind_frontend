d96368eecf2c2d8c27504fbce7f019d1098c9f35
neto <euneto2501@gmail.com>
2023-11-23 01:23:05 -0300

    estrutura inicial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
